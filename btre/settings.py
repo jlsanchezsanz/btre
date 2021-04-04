538a859438d9671ca14116149409e688260c155b
jlsanchezsanz <jlsanchezsanz@hotmail.com>
2021-04-04 09:00:34 +0200

    chore: set up media folder

--- btre/settings.py
+++ btre/settings.py
@@ -128,3 +128,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     Path.joinpath(BASE_DIR, 'btre/static')
 ]
+
+# Media Folder Settings
+MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
